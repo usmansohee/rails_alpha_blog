069e4bed91a3de2f19676fcbaee4e5010aa99c95
usmansohee <uxmansohee@gmail.com>
2022-08-25 17:41:13 +0500

    added category table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_17_113328) do
+ActiveRecord::Schema.define(version: 2022_08_25_123903) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title", null: false
@@ -19,6 +19,10 @@ ActiveRecord::Schema.define(version: 2022_08_17_113328) do
     t.datetime "updated_at"
     t.integer "user_id"
     t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
   end
 
   create_table "users", force: :cascade do |t|
